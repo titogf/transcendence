f8f871709bfe574b1f5a15202db578b0ea9b572d
Carlos Eugenio Martin Gutierrez <carlos-m@c2r14s5.42madrid.com>
2025-04-16 15:52:40 +0200

    prueba

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Pong from "./game/Pong"; // Aquí pondrás el juego
+import Pong from "./game/Pong";
 
 function App() {
   return (
