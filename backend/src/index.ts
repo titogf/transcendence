f6cfa8c285dacd60550b2a0b583d388646c0818a
Carlos Eugenio Martin Gutierrez <carlos-m@c2r8s3.42madrid.com>
2025-05-03 16:33:07 +0200

    añadido https a frontend :)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({ origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://10.12.15.1:5173"], credentials: true}));
+app.use(cors({ origin: ["https://localhost:5173",
+  "https://127.0.0.1:5173",
+  "https://10.12.15.1:5173"],
+  credentials: true}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
