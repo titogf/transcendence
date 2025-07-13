132b99649fe1f9f3f27c0f3aa1ec5eea062ad94d
Carlos Eugenio Martin Gutierrez <carlos-m@c1r9s3.42madrid.com>
2025-07-13 13:46:07 +0200

    https en frontend corriendo perfecto

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,8 @@ const authRoutes = require('./routes/auth');
 
 fastify.register(require('@fastify/cors'), {
   origin: [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173'
+    'https://localhost:5173',
+    'https://127.0.0.1:5173'
   ],
   credentials: true
 });
--- frontend/server.js
+++ frontend/server.js
@@ -1,16 +1,25 @@
-const fastify = require("fastify")({ logger: true });
+const fs = require("fs");
 const path = require("path");
+
+const fastify = require("fastify")({
+  logger: true,
+  https: {
+    key: fs.readFileSync(path.join(__dirname, "certs", "key.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "certs", "cert.pem")),
+  },
+});
+
 const fastifyStatic = require("@fastify/static");
 
 // dist = estilos generados
-fastify.register(require("@fastify/static"), {
+fastify.register(fastifyStatic, {
   root: path.join(__dirname, "dist"),
   prefix: "/dist/",
   decorateReply: false,
 });
 
 // public = html + js
-fastify.register(require("@fastify/static"), {
+fastify.register(fastifyStatic, {
   root: path.join(__dirname, "public"),
   prefix: "/",
 });
@@ -22,9 +31,8 @@ fastify.get("/", (req, reply) => {
 
 const start = async () => {
   try {
-    // 👇 ESTA ES LA LÍNEA IMPORTANTE
-    await fastify.listen({ port: 5173, host: '0.0.0.0' });
-    console.log("🧑‍🎨 Frontend corriendo en http://localhost:5173");
+    await fastify.listen({ port: 5173, host: "0.0.0.0" });
+    console.log("🧑‍🎨 Frontend corriendo en https://localhost:5173");
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
