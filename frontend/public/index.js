7aefd715ff23e2b75c9d28ea74f8141d50080560
Gonzalo <gonchogfa@gmail.com>
2025-07-18 19:25:45 +0200

    maxlength users

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -4,6 +4,7 @@ window.addEventListener("DOMContentLoaded", () => {
     const loginBtn = document.getElementById("singin-btn");
     const tournamentBtn = document.getElementById("tournament-btn");
     const play4Btn = document.getElementById("play4-btn");
+    const userAvatar = document.getElementById("user-avatar");
     const userStr = localStorage.getItem("user");
     if (userStr && loginBtn) {
         const user = JSON.parse(userStr);
@@ -12,6 +13,18 @@ window.addEventListener("DOMContentLoaded", () => {
         loginBtn.onclick = () => {
             window.location.href = "./profile.html";
         };
+
+        fetch(`http://localhost:3000/auth/user-info/${user.username}`)
+            .then(res => res.json())
+            .then(userData => {
+            const avatarIndex = userData.avatar >= 0 && userData.avatar <= 9 ? userData.avatar : 0;
+            if (userAvatar) {
+                userAvatar.src = `/avatars/${avatarIndex}.png`;
+            }
+        })
+            .catch(err => {
+            console.error("Error cargando avatar del usuario:", err);
+        });
     }
     else if (loginBtn) {
         loginBtn.textContent = "Sign in/Register";
@@ -25,12 +38,12 @@ window.addEventListener("DOMContentLoaded", () => {
         });
     }
     if (tournamentBtn) {
-        tournamentBtn.addEventListener("click", () => {
+        tournamentBtn === null || tournamentBtn === void 0 ? void 0 : tournamentBtn.addEventListener("click", () => {
             window.location.href = "./tournament.html";
         });
     }
     if (play4Btn) {
-        play4Btn.addEventListener("click", () => {
+        play4Btn === null || play4Btn === void 0 ? void 0 : play4Btn.addEventListener("click", () => {
             window.location.href = "./play4.html";
         });
     }
--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -3,8 +3,10 @@ window.addEventListener("DOMContentLoaded", () => {
   const loginBtn = document.getElementById("singin-btn") as HTMLButtonElement | null;
   const tournamentBtn = document.getElementById("tournament-btn") as HTMLButtonElement | null;
   const play4Btn = document.getElementById("play4-btn") as HTMLButtonElement | null;
+  const userAvatar = document.getElementById("user-avatar") as HTMLImageElement | null;
 
   const userStr = localStorage.getItem("user");
+
   if (userStr && loginBtn) {
     const user = JSON.parse(userStr);
     loginBtn.textContent = user.username;
@@ -12,6 +14,18 @@ window.addEventListener("DOMContentLoaded", () => {
     loginBtn.onclick = () => {
       window.location.href = "./profile.html";
     };
+
+    fetch(`http://localhost:3000/auth/user-info/${user.username}`)
+      .then(res => res.json())
+      .then(userData => {
+        const avatarIndex = userData.avatar >= 0 && userData.avatar <= 9 ? userData.avatar : 0;
+        if (userAvatar) {
+          userAvatar.src = `/avatars/${avatarIndex}.png`;
+        }
+      })
+      .catch(err => {
+        console.error("Error cargando avatar del usuario:", err);
+      });
   } else if (loginBtn) {
     loginBtn.textContent = "Sign in/Register";
     loginBtn.onclick = () => {
@@ -26,13 +40,13 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 
   if (tournamentBtn) {
-    tournamentBtn.addEventListener("click", () => {
+    tournamentBtn?.addEventListener("click", () => {
       window.location.href = "./tournament.html";
     });
   }
 
   if (play4Btn) {
-    play4Btn.addEventListener("click", () => {
+    play4Btn?.addEventListener("click", () => {
       window.location.href = "./play4.html";
     });
   }
